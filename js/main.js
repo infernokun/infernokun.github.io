65c583135e70b287304794bbf70a859abcf3e3ea
infernokun <sasukeucheha@live.com>
2021-02-22 16:13:58 -0600

    removed scroll function for now

--- js/main.js
+++ js/main.js
@@ -49,36 +49,6 @@ const navBar = document.querySelector('.nav');
 const navLogo = document.querySelector('.nav-logo');
 const showMore = document.querySelector('.show-more');
 
-const navHeight = navBar.getBoundingClientRect().height;
-window.addEventListener('scroll', () => {
-    const scrollHeight = window.pageYOffset;
-    if (scrollHeight > navHeight) {
-        navBar.classList.add("fix-nav");
-    } else {
-        navBar.classList.remove("fix-nav");
-    }
-});
-
-const links = [...document.querySelectorAll(".scroll-link")];
-
-links.map(link => {
-    link.addEventListener('click', e => {
-        e.preventDefault();
-
-        const id = e.target.getAttribute('href').slice(1);
-        const el = document.getElementById(id);
-        let position = el.offsetTop - navHeight;
-
-        window.scrollTo({
-            top: position,
-            left: 0,
-        });
-        navBar.classList.remove('show');
-        menu.classList.remove('show');
-        document.body.classList.remove('show');
-    });
-});
-
 showMore.addEventListener('click', () => {
     pushAllProjects();
 
